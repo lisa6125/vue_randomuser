8a6938ba1762b6e19bfaac51f05acc62f348bb4f
lisa6125 <fxck6125@gmail.com>
2022-02-15 14:01:34 +0800

    分頁元件拆分

---
+++ src/components/Pagination.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+      <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px mb-12" aria-label="Pagination">
+        <a v-show="nowPage>1" class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50" @click="pageChang(-1)">
+          <span class="sr-only">Previous</span>
+          <!-- Heroicon name: solid/chevron-left -->
+          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+          </svg>
+        </a>
+        <!-- Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" -->
+        <a aria-current="page" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium" :class="{'z-10 bg-indigo-50 border-indigo-500 text-indigo-600':nowPage===1}" @click="clickPage(1)"> 1 </a>
+        <a v-show="nowPage<4" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium" :class="{'z-10 bg-indigo-50 border-indigo-500 text-indigo-600 hover:bg-indigo-50':nowPage===2}" @click="clickPage(2)"> 2 </a>
+        <a v-show="nowPage<4" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium" :class="{'z-10 bg-indigo-50 border-indigo-500 text-indigo-600 hover:bg-indigo-50':nowPage===3}" @click="clickPage(3)"> 3 </a>
+        <span class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700"> ... </span>
+        <a v-show="nowPage>=4 && nowPage<9" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium" @click="clickPage(nowPage -1)">{{nowPage -1}} </a>
+        <a v-show="nowPage>=4 && nowPage<9" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium z-10 bg-indigo-50 border-indigo-500 text-indigo-600 hover:bg-indigo-50">{{nowPage}} </a>
+        <a v-show="nowPage>=4 && nowPage<9" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium" @click="clickPage(nowPage +1)">{{nowPage +1}} </a>
+        <span v-show="nowPage>=4 && nowPage<9" class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700"> ... </span>
+        <a v-show="nowPage>8" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium" :class="{'z-10 bg-indigo-50 border-indigo-500 text-indigo-600 hover:bg-indigo-50':nowPage===9}" @click="clickPage(9)"> 9 </a>
+        <a v-show="nowPage>8" class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium" :class="{'z-10 bg-indigo-50 border-indigo-500 text-indigo-600 hover:bg-indigo-50':nowPage===10}" @click="clickPage(10)"> 10 </a>
+        <a class="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium" :class="{'z-10 bg-indigo-50 border-indigo-500 text-indigo-600 hover:bg-indigo-50':nowPage===11}" @click="clickPage(11)"> 11 </a>
+        <a v-show="nowPage<11" class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50" @click="pageChang(1)">
+          <span class="sr-only">Next</span>
+          <!-- Heroicon name: solid/chevron-right -->
+          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+          </svg>
+        </a>
+      </nav>
+    </div>
+</template>
+
+<script>
+export default {
+  props:['nowPage'],
+  setup(props, target){
+    function clickPage(page){
+      target.emit("clickPage",page);
+    }
+    function pageChang(num){
+      target.emit("pageChang",num);
+    }
+    return {clickPage,pageChang}
+  }
+}
+</script>
+
+<style>
+
+</style>
